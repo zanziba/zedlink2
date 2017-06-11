57e25455e59fa544115f8c3f4befe353c4c1a299
zanziba <en.mostafa.goravani@gmail.com>
2017-06-11 14:25:05 +0430

    Create utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -214,7 +214,7 @@ end
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
 -- do ski
-msg_caption = '\n@'..string.reverse("maeTdnoyeB")
+msg_caption = '\n@'..string.reverse("bankknil")
 -- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
